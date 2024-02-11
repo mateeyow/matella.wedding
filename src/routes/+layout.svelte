ebee3f1bbf823d511ec8e7ac9c6c9ca2ca3e881b
Matthew Torres <matthew.torres211@gmail.com>
2024-02-11 22:51:53 +0800

    Change og image link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
 	import '../app.css';
+	import bgImage from '$lib/assets/hero-bg.png';
 </script>
 
 <svelte:head>
 	<title>Matt & Ella's Wedding</title>
 	<meta name="description" content="Matt and Ella's wedding website" />
-	<!-- Generate og tags -->
 	<meta property="og:title" content="Matt & Ella's Wedding" />
 	<meta property="og:description" content="Matt and Ella's wedding website" />
-	<meta property="og:image" content="$lib/assets/hero-bg.png" />
+	<meta property="og:image" content={bgImage} />
 	<meta property="og:url" content="https://mattella-wedding.vercel.app" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="Matt & Ella's Wedding" />
