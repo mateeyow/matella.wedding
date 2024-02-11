0e89983bf93fb97dd4efa4ad661ae758f6a92d88
Matthew Torres <matthew.torres211@gmail.com>
2024-02-11 22:48:33 +0800

    Add og metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,16 @@
 </script>
 
 <svelte:head>
-	<title>MattElla Wedding</title>
+	<title>Matt & Ella's Wedding</title>
 	<meta name="description" content="Matt and Ella's wedding website" />
+	<!-- Generate og tags -->
+	<meta property="og:title" content="Matt & Ella's Wedding" />
+	<meta property="og:description" content="Matt and Ella's wedding website" />
+	<meta property="og:image" content="$lib/assets/hero-bg.png" />
+	<meta property="og:url" content="https://mattella-wedding.vercel.app" />
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="Matt & Ella's Wedding" />
+	<meta property="og:locale" content="en_US" />
 </svelte:head>
 
 <slot />
