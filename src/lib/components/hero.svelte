4ba2bc5cf39a8a99f322c38b3dc12aea37d0039a
Matthew Torres <matthew.torres211@gmail.com>
2024-02-12 23:32:12 +0800

    Add save date

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,7 +1,4 @@
-<div
-	class="bg-[url('$lib/assets/hero-bg.png')] h-svh text-white bg-center bg-cover flex flex-col"
-	id="save-the-date"
->
+<div class="bg-[url('$lib/assets/hero-bg.png')] h-svh text-white bg-center bg-cover flex flex-col">
 	<div class="m-auto text-center">
 		<h1 class="text-[80px]">Matthew <span class="text-[53px]">and</span> Ella</h1>
 		<h2>03 . 01 . 2025 | Villa Plenilunio, Uluwatu Bali</h2>
---
+++ src/lib/components/save.svelte
@@ -0,0 +1,24 @@
+<div id="save-the-date" class="grid-cols-2">
+	<div>
+		<h3>Save the date</h3>
+		<p>Hi [name],</p>
+		<p>
+			With the sound of the waves and the sunset at our backs, we are beyond thrilled to extend our
+			heartfelt invitation to you.
+		</p>
+		<p>
+			Our special day promises a day filled with laughter, sunkissed skin, bottoms up, after-wedding
+			hangover and most especially a memory that will linger in our hearts for a lifetime.
+		</p>
+		<p>
+			Please join us in this "intimate but rowdy" celebration of our love. You are special to us,
+			and we hope to share this moment with you like how you have shared your special moments with
+			us.
+		</p>
+		<p>
+			We are looking forward to celebrating with you. The courtesy of a reply is requested by May
+			15, 2024.
+		</p>
+	</div>
+	<div>Second</div>
+</div>
