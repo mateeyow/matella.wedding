186f89b518c381e90428a289150b845ab0931bd9
Matthew Torres <matthew.torres211@gmail.com>
2024-02-17 17:18:25 +0800

    Semi finish the venue section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}', "./node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}",],
   theme: {
     extend: {
       colors: {
         bgLight: '#E9E7E1',
+        bgDark: '#D7D4C9',
         black: '#1B1B1B'
       },
       screens: {
