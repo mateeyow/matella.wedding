e24e341475cbe12e88b886d91846d24e5aa78961
Matthew Torres <matthew.torres211@gmail.com>
2024-02-28 00:21:55 +0800

    Add effects on hover

--- src/lib/components/image-break.svelte
+++ src/lib/components/image-break.svelte
@@ -3,7 +3,19 @@
 	import BreakImage2 from '$lib/assets/images/break-img-2.png';
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2">
-	<img src={BreakImage1} alt="Matt and Ella sitting" />
-	<img src={BreakImage2} alt="Matt and Ella laughing while walking" />
+<div class="grid grid-cols-1 md:grid-cols-2 overflow-hidden">
+	<div class="overflow-hidden">
+		<img
+			src={BreakImage1}
+			alt="Matt and Ella sitting"
+			class="hover:scale-110 transition-transform ease-in duration-300"
+		/>
+	</div>
+	<div class="overflow-hidden">
+		<img
+			src={BreakImage2}
+			alt="Matt and Ella laughing while walking"
+			class="hover:scale-110 transition-transform ease-in duration-300"
+		/>
+	</div>
 </div>
